2fdf4efe3955bb7f5373ad4e7174a039a2248172
gabicilfone <cilfonegabriel@gmail.com>
2024-02-29 00:51:39 -0300

    Create schema in product.js

--- backend/controllers/schemas/products.js
+++ backend/controllers/schemas/products.js
@@ -15,4 +15,23 @@ const getProductsSchema = {
     },
 }};
 
-module.exports = { getProductsSchema };
+const getProductSchema = {
+    params: {
+      id: { type: 'number' },
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          id: { type: 'number' },
+          name: { type: 'string' },
+          description: { type: 'string' },
+          price: { type: 'number' },
+          availability: { type: 'boolean' }
+        },
+      },
+    },
+  };
+  
+
+module.exports = { getProductsSchema, getProductSchema };
