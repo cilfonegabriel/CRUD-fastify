889def84f3c4b77f32a9211e16104f49e1834d2f
gabicilfone <cilfonegabriel@gmail.com>
2024-02-29 00:34:50 -0300

    Handling responses to our publishing routes.

--- backend/controllers/handlers/products.js
+++ backend/controllers/handlers/products.js
@@ -1,9 +1,7 @@
+const products = require('../../db/products.js');
+
 const getProductsHandler = (req, reply) => {
-    reply.send([
-        { id: 1, name: 'Product One', description: 'This is product one', price: 30.20, availability: true },
-        { id: 2, name: 'Product Two', description: 'This is product two', price: 19.50, availability: false },
-        { id: 3, name: 'Product Three', description: 'This is product three', price: 8.99, availability: true }
-    ]);
+    reply.send(products);
   };
   
   module.exports = { getProductsHandler };
