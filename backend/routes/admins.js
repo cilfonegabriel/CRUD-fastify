1751394f0a076804eaf72606b9cc881582b785ae
gabicilfone <cilfonegabriel@gmail.com>
2024-02-29 02:25:55 -0300

    Add route and import handler and schema of admins

--- backend/routes/admins.js
+++ backend/routes/admins.js
@@ -1,5 +1,15 @@
-const adminRoutes = (fastify, options) => {
+const { getAdminsHandler } =  require('../controllers/handlers/admins.js');
+const { getAdminsSchema } = require('../controllers/schemas/admins.js');
 
-  };
+const getAdminsOpts = {
+    schema: getAdminsSchema,
+    handler: getAdminsHandler,
+};
+
+const adminRoutes = (fastify, options, done) => {
+    fastify.get('/api/admins', getAdminsOpts);
+
+      done();
+};
   
   module.exports = adminRoutes;
