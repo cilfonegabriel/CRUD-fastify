c016ac11e390863d55ac7206a70e94ccf48d3e0b
gabicilfone <cilfonegabriel@gmail.com>
2024-02-29 00:35:41 -0300

    We specify the schemas of our publication routes.

---
+++ backend/db/products.js
@@ -0,0 +1,9 @@
+const products = [
+    { id: 1, name: 'Product One', description: 'This is product one', price: 5.99, availability: true },
+    { id: 2, name: 'Product Two', description: 'This is product two', price: 10.15, availability: false },
+    { id: 3, name: 'Product Three', description: 'This is product three', price: 20.75, availability: true },
+    { id: 4, name: 'Product Four', description: 'This is product four', price: 100, availability: true },
+    { id: 5, name: 'Product Five', description: 'This is product five', price: 14.10, availability: false }
+];
+
+module.exports = products;
